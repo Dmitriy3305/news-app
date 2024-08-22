8bca9fcb0029cae26348ed6edd647b322c98237e
dmitrij <dmitrijkrymskij6@gmail.com>
2024-08-23 00:11:26 +0400

    fix: topheadlines

--- src/components/TopHeadLines/TopHeadlines.tsx
+++ src/components/TopHeadLines/TopHeadlines.tsx
@@ -6,7 +6,7 @@ import {
   selectHeadlines,
   selectLoading,
 } from '../../store/slices/newsSlice';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchNews } from '../../api/fetchNews';
 
 const TopHeadlines: React.FC = (): JSX.Element => {
@@ -14,12 +14,17 @@ const TopHeadlines: React.FC = (): JSX.Element => {
   const loading = useAppSelector(selectLoading);
   const error = useAppSelector(selectError);
   const topHeadlines = useAppSelector(selectHeadlines);
+  const [previousHeadlines, setPreviousHeadlines] = useState<ISources[]>([]);
 
   useEffect(() => {
     dispatch(fetchNews({ dispatch: dispatch, activeMenu: null, item: null }));
   }, [dispatch]);
 
-  if (loading) return <div>Loading...</div>;
+  useEffect(() => {
+    setPreviousHeadlines(topHeadlines.slice(0, 5));
+  }, [topHeadlines]);
+
+  if (loading) return <div></div>;
   if (error) return <div>Error: {error}</div>;
 
   return (
@@ -28,7 +33,10 @@ const TopHeadlines: React.FC = (): JSX.Element => {
       {topHeadlines.length > 0 ? (
         <ul>
           {topHeadlines.slice(0, 5).map((sources: ISources, index) => (
-            <li className={styles['article']} key={index}>
+            <li
+              className={`${styles['article']} ${styles['slide-in']}`}
+              key={index}
+            >
               <a
                 href={sources.url}
                 target="_blank"
@@ -40,6 +48,23 @@ const TopHeadlines: React.FC = (): JSX.Element => {
               </a>
             </li>
           ))}
+          {previousHeadlines.length > 0 &&
+            previousHeadlines.map((sources: ISources, index) => (
+              <li
+                className={`${styles['article']} ${styles['slide-out']}`}
+                key={index + 10}
+              >
+                <a
+                  href={sources.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles['article-link']}
+                >
+                  <p className={styles['description']}>{sources.description}</p>
+                  <p className={styles['name']}>{sources.name}</p>
+                </a>
+              </li>
+            ))}
         </ul>
       ) : (
         <p>No headlines available</p>
